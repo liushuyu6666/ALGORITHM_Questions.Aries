344b2d8fd9ba512f69481767d792fce8202968cc
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-03-26 19:31:51 -0400

    find the intersection, manipulate the linked list

---
+++ java/src/Intersection_of_Two_Linked_Lists.java
@@ -0,0 +1,42 @@
+public class Intersection_of_Two_Linked_Lists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB){
+        int countA = 0, countB = 0;
+        ListNode lA = headA, lB = headB;
+        while(lA != null){
+            countA++;
+            lA = lA.next;
+        }
+        while(lB != null){
+            countB++;
+            lB = lB.next;
+        }
+        if(countA == 0 || countB == 0){
+            return null;
+        }
+        lA = headA;
+        lB = headB;
+        if(countA >= countB){
+            int diff = countA - countB;
+            while(diff > 0){
+                lA = lA.next;
+                diff--;
+            }
+            while(lA != lB){
+                lA = lA.next;
+                lB = lB.next;
+            }
+        }
+        if(countB >= countA){
+            int diff = countB - countA;
+            while(diff > 0){
+                lB = lB.next;
+                diff--;
+            }
+            while(lA != lB){
+                lA = lA.next;
+                lB = lB.next;
+            }
+        }
+        return lA;
+    }
+}
