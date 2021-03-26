f91bd2839e7160a11af3cb85659b949d92071fb6
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-03-26 18:31:26 -0400

    manipulate with ListNode

---
+++ java/src/Odd_Even_Linked_List.java
@@ -0,0 +1,29 @@
+public class Odd_Even_Linked_List {
+    public ListNode oddEvenList(ListNode head){
+        ListNode curr = head;
+        boolean isOdd = true;
+        ListNode oddHeader = new ListNode();
+        ListNode oddCurr = oddHeader;
+        ListNode evenHeader = new ListNode();
+        ListNode evenCurr = evenHeader;
+        while(curr != null){
+            if(isOdd){
+                oddCurr.next = new ListNode();
+                oddCurr = oddCurr.next;
+                oddCurr.val = curr.val;
+                isOdd = false;
+            }
+            else{
+                evenCurr.next = new ListNode();
+                evenCurr = evenCurr.next;
+                evenCurr.val = curr.val;
+                isOdd = true;
+            }
+            curr = curr.next;
+        }
+        if(oddHeader.next != null){
+            oddCurr.next = evenHeader.next;
+        }
+        return oddHeader.next;
+    }
+}
