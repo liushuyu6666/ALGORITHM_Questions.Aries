e28ffa738e50db7ace044350a996d0d7b836735c
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-04-30 23:29:04 -0400

    bit manipulation

---
+++ java/src/BitManipulation/Bit.java
@@ -0,0 +1,37 @@
+package BitManipulation;
+
+public class Bit {
+
+    public static void main(String args[]){
+
+        // arithmetic shift(>>) and logical shift(>>>)
+        System.out.println(-4>>1);
+        System.out.println(-4>>>1);
+        System.out.println(-4>>1);
+        int ans = -4;
+        for(int i = 0; i < 10; i++){
+            ans <<= 1;
+            System.out.println(ans);
+        }
+        // no <<<
+        System.out.println(Integer.toBinaryString(-4>>1));
+        System.out.println(Integer.toBinaryString(-4>>>1));
+
+        // bit wise and
+        System.out.println(4 & 5); // 100 & 101
+
+        // bit wise or
+        System.out.println(4 | 5); // 100 | 101
+
+        // bit wise XOR
+        System.out.println(4 ^ 5); // 100 ^ 101 = 001
+
+        // bit wise XNOR
+        System.out.println(~(4 ^ 5)); // ~(100 ^ 101) = 110
+        // bit wise not
+        System.out.println(~4); // 100 => 011
+
+
+    }
+
+}
