441799b5eeb70dfb141f86a3ff8cff36b93137e1
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-05-05 00:41:10 -0400

    add add(index, element) method and a new way to copy

--- java/src/DataStructure/Basic_ArrayList.java
+++ java/src/DataStructure/Basic_ArrayList.java
@@ -8,21 +8,31 @@ import java.util.List;
 public class Basic_ArrayList {
     public static void main(String args[]){
 
-        // 1, new
+        // 1,1 new
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
         System.out.println(list);
+
+        // 1.2 copy or new
+        List<Integer> newList = new ArrayList<>(list);
 
         // 2, subList
         List<Integer> subList = list.subList(0, 3);
         System.out.println(subList);
 
-        // 3.1 create nested ArrayList
+        // 3, add with index
+        // list can't add since it generate by Arrays.asList
+//        list.add(1); // UnsupportedOperationException
+        newList.add(9, 10);
+        System.out.println(newList);
+
+        // 4.1 create nested ArrayList
         int[][] arr = new int[][]{{1, 1}, {2, 1}, {2, 2}, {3, 4}};
         List<int[]> arrList = new ArrayList<>();
         Collections.addAll(arrList, arr);
 
-        // 3.2 contains, pay attention, the contains will check the pointer
+        // 4.2 contains, pay attention, the contains will check the pointer
         int[] target = arr[0];
         System.out.println(arrList.contains(target));
+
     }
 }
